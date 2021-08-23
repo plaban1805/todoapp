3088ffd1d97846da79bbae393aef6d865e60ab87
Plaban Das <p-das@ti.com>
2021-08-23 12:39:32 +0530

    docker compose working

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 
 mongoose.connect(
-  'mongodb://localhost:27017/tasks',
+  'mongodb://mongodb:27017/tasks',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
